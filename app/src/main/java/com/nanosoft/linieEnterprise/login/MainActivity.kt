c3b265fe033e79b9d85797d950c31e48041adb04
Vijay Chalwadi <vijay.chalwadi@neosofttech.com>
2019-12-03 15:17:48 +0530

    added git ignore file

---
+++ app/src/main/java/com/nanosoft/linieEnterprise/login/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.nanosoft.linieEnterprise.login
+
+import com.nanosoft.linieEnterprise.BaseActivity
+import com.nanosoft.linieEnterprise.R
+
+class MainActivity : BaseActivity() {
+
+    override fun init() {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.activity_main
+    }
+
+}
